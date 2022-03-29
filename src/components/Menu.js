5dc7569a190f1d1a8da9996c09ab1a0a40282e87
Simon Dupkala <simon.dupkala@gmail.com>
2022-03-29 21:01:01 +0100

    added active

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Menu = () => {
   return (
@@ -29,17 +29,17 @@ const Menu = () => {
     General
   </p>
   <ul className="menu-list">
-  <li><Link to="/">Home</Link></li>
-<li><Link to="/recipes">Recipes</Link></li>
+  <li><NavLink activeclassname="active" to="/">Home</NavLink></li>
+<li><NavLink activeclassname="active" to="/recipes">Recipes</NavLink></li>
   </ul>
   <p className="menu-label">
     Administration
   </p>
   <ul className="menu-list">
-    <li><Link to="/mealplan">Meal plan</Link></li>
+    <li><NavLink activeclassname="active" to="/mealplan">Meal plan</NavLink></li>
     <li>
-    <Link to="/account">Account</Link></li>
-    <li><Link to="/search">Search Food Nutrition</Link></li>
+    <NavLink activeclassname="active" to="/account">Account</NavLink></li>
+    <li><NavLink activeclassname="active" to="/search">Search Food Nutrition</NavLink></li>
   </ul>
 </aside>
   )
