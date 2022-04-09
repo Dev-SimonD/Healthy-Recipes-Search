4c602ff4f2bd4070cdff174763f091f031badbc0
Simon Dupkala <simon.dupkala@gmail.com>
2022-04-09 20:19:54 +0100

    initial

--- src/pages/MealPlan.jsx
+++ src/pages/MealPlan.jsx
@@ -26,10 +26,10 @@ const MealPlan = ({session}) => {
   const [spoonPassword, setSpoonPassword] = useState(null)
   const [spoonHash, setSpoonHash] = useState(null)
   const [mealPlan, setMealPlan] = useState()
-  const [mealPlanNutrients, setMealPlanNutrients] = useState()
+  const [mealPlanNutrients, setMealPlanNutrients] = useState(null)
   const [bmrValue, setBmrValue] = useState()
   const [coef, setCoef] = useState()
-  const [calories, setCalories] = useState(0)
+  const [tdeeValue, setTdeeValue] = useState(0)
 
 
    useEffect(() => {
@@ -43,7 +43,7 @@ const MealPlan = ({session}) => {
    
          let { data, error, status } = await supabase
            .from('profiles')
-           .select(`username, updated, spoonUsername, spoonPassword, spoonHash, bmrValue, coef `)
+           .select(`username, updated, spoonUsername, spoonPassword, spoonHash, bmrValue, coef, tdeeValue `)
            .eq('id', user.id)
            .single()
    
@@ -59,28 +59,14 @@ const MealPlan = ({session}) => {
            setSpoonHash(data.spoonHash)
            setBmrValue(data.bmrValue)
            setCoef(data.coef)
+           setTdeeValue(data.tdeeValue)
           
          }
           
-        // console.log(breakfast)
-        
-          /* setCalories((bmrValue * coef).toFixed())
-          console.log(calories) */
-        const response = await fetch(`https://api.spoonacular.com/mealplanner/generate?timeFrame=day&targetCalories=${2000}&apiKey=${process.env.REACT_APP_API_KEY}`)
-    const spoonData = await response.json()
-    setMealPlanNutrients(spoonData.nutrients)
-    console.log(spoonData.nutrients)
-    //console.log(spoonData.meals[0].id);
-    /* setBreakfastId(spoonData.meals[0].id)
-    setLaunchId(spoonData.meals[1].id)
-    setDinnerId(spoonData.meals[2].id) */
          
-         const meals = await fetch(`https://api.spoonacular.com/recipes/informationBulk?ids=${spoonData.meals[0].id},${spoonData.meals[1].id},${spoonData.meals[2].id}&apiKey=${process.env.REACT_APP_API_KEY}`)
-         const mealsData = await meals.json()
-        // console.log(mealsData[2])
-        setBreakfast(mealsData[0])
-        setLaunch(mealsData[1])
-        setDinner(mealsData[2])
+         
+      
+       
 
        } catch (error) {
          alert(error.message)
@@ -115,15 +101,41 @@ const MealPlan = ({session}) => {
     setMealplanstate(data)
 
 } */
+
+  
+
   const clickHandler = ((e) => {
     e.preventDefault()
     console.log("clicked")
   
   })
+
+  const handleClick = ((e) => {
+    e.preventDefault()
+    console.log("tdee on click", tdeeValue)
+    getMealPlanNow()
+   
+  })
   
+  const getMealPlanNow = async () => {
+    console.log("tdee value before apicall",tdeeValue)
+
+    const response = await fetch(`https://api.spoonacular.com/mealplanner/generate?timeFrame=day&targetCalories=${tdeeValue}&apiKey=${process.env.REACT_APP_API_KEY}`)
+    const spoonData = await response.json()
+    setMealPlanNutrients(spoonData.nutrients)
+    console.log(spoonData.nutrients)
+
+         const meals = await fetch(`https://api.spoonacular.com/recipes/informationBulk?ids=${spoonData.meals[0].id},${spoonData.meals[1].id},${spoonData.meals[2].id}&apiKey=${process.env.REACT_APP_API_KEY}`)
+         const mealsData = await meals.json()
+         console.log(mealsData)
+        setBreakfast(mealsData[0])
+        setLaunch(mealsData[1])
+        setDinner(mealsData[2])
+        console.log(dinner)
+  }
 
   return (
-    <div>
+    <div className='container'>
       {loading ? (<div style={{"display":"flex", "justifyContent":"center", "alignItems":"center"}}><img src={loadingGif} alt="loading"/></div>):(
         <div>
     <div className={!updated ? "" : "nonDisplay" }>
@@ -136,25 +148,33 @@ const MealPlan = ({session}) => {
    </div>
    </div>
         <div className={updated ? "" : "nonDisplay" }>
-           <h1>Meal plan for a day</h1>
+          <button className='button is-primary signupBtn' onClick={handleClick}>find recipe</button>
+           <h1 className='title'>Daily meal plan for your individual goals</h1>
+           {mealPlanNutrients ? (
+             <div>
+           <p className='label'>{`Your healthy calories intake is ${tdeeValue}kcal`}</p>
            <ul>
-             <li>calories: {mealPlanNutrients.calories}</li>
-             <li>carbs: {mealPlanNutrients.carbohydrates}</li>
-             <li>fats: {mealPlanNutrients.fat}</li>
-             <li>proteins: {mealPlanNutrients.protein}</li>
+             <li>calories: {mealPlanNutrients ? (mealPlanNutrients.calories) : ("")}</li>
+             <li>carbs: {mealPlanNutrients ? (mealPlanNutrients.carbohydrates) : ("")}</li>
+             <li>fats: {mealPlanNutrients ? (mealPlanNutrients.fat) : ("")}</li>
+             <li>proteins: {mealPlanNutrients ? (mealPlanNutrients.protein) : ("")}</li>
            </ul>
-    <div className='recipesCard'>     
-           <h2>{`Breakfast: `} {breakfast != null ? (breakfast.title) : ""}</h2>  
-{/*            <img src={breakfast.image} alt={breakfast.title}/>                           
- */}      </div>
-      <div className='recipesCard'> 
-      <h2>{`Launch: `} {launch != null ? (launch.title) : ""}</h2>  
-{/*            <img src={launch.image} alt={launch.title}/>
- */}     </div>
-      <div className='recipesCard'>           
-      <h2>{`Dinnere: `} {dinner != null ? (dinner.title) : ""}</h2>  
-{/*         <img src={dinner.image} alt={dinner.title}/>
- */}    </div>
+           <div className='row'>
+              <div className='recipesCard col-3'>     
+                    <h2>{`Breakfast: `} {breakfast != null ? (breakfast.title) : ""}</h2>  
+                    {breakfast ? (<img src={breakfast.image} alt={breakfast.title}/>):("")}                           
+                </div>
+                <div className='recipesCard col-3'> 
+                <h2>{`Launch: `} {launch != null ? (launch.title) : ""}</h2>  
+                {launch ? (<img src={launch.image} alt={launch.title}/>):("")}
+              </div>
+                <div className='recipesCard col-3'>           
+                <h2>{`Dinner: `} {dinner != null ? (dinner.title) : ""}</h2>  
+                  {dinner ? (<img src={dinner.image} alt={dinner.title}/>):("")}
+              </div>
+              </div>
+    </div>
+    ) : ("")}
     </div>
     </div>
     )}
