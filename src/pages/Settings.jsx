96647adf9bf6026f2c9e3cf9fcd98394ac2423c3
Simon Dupkala <simon.dupkala@gmail.com>
2022-05-02 02:42:43 +0100

    update

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -5,15 +5,17 @@ import { supabase } from '../components/supabaseClient'
 const Settings = () => {
   return (
     <div>
-      <div className='accountForm' id='settingsMain'>
+      <div className='accountForm' id='mealPlanAccountForm'>
         <h1 className='title has-text-centered'>Settings</h1>
         <div className='settingsButtons'>
+        <div>
         <p>Metric vs Imperial</p>
         <button>Metric</button>
         <p>Toggle dark mode</p>
         <button>Theme</button>
         <p>Change password</p>
         <button>Password</button>
+        </div>
         <button type="button" className="button settingsButton" onClick={() => supabase.auth.signOut()}>
         Sign Out
       </button>
